49a4639e2239af5098ba3becbd5bd18d0af67f49
Stanislav <kunitskiisv@gmail.com>
2025-03-18 13:05:35 +0300

    Lint fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,4 @@
 
 %w[Новости Развлечения Спорт Технологии].map { |name| Category.create!(name: name) }
 
-User.create!(email: 'admin@example.com', password: 'password') unless User.exists?
-
 Rails.logger.debug 'Сущности успешно созданы!'
